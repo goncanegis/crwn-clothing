869252c261a4f843afaee35b3bd60709ead6a1a2
Gonca Negis <gonca.negis@gmail.com>
2022-07-13 21:02:02 +0300

    created sign up component, form input, button component

---
+++ src/components/form-input/FormInput.js
@@ -0,0 +1,21 @@
+import './FormInput.scss';
+import React from 'react';
+
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <div className="group">
+      <input {...otherProps} className="form-input" />
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? 'shrink' : ''
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
